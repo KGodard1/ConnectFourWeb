4deaa17a3cb90941c3325b4ad8c779be10f0381a
Karl Godard <godardk@umich.edu>
2020-08-08 12:35:43 -0400

    Deleted unnecessary decorators

--- Main.py
+++ Main.py
@@ -104,11 +104,13 @@ def makemove():
 
 		
 		if checkForWin(board, move[0], move[1]):
-			win_data = {"move_made":True, "phase": "end", "outcome": turnToMove, "board": board}
+			r.hset(gameData,"phase", "post-game")
+			win_data = {"move_made":True, "phase": "post-game", "outcome": turnToMove, "board": board}
 			win_json = json.dumps(win_data)
 			return win_json
 		elif checkForTie(board):
-			tie_data = {"move_made":True, "phase":"end", "outcome":"tie", "board": board}
+			r.hset(gameData,"phase", "post-game")
+			tie_data = {"move_made":True, "phase":"post-game", "outcome":"tie", "board": board}
 			tie_json = json.dumps(tie_data)
 			return tie_json
 		else:
@@ -127,62 +129,6 @@ def getUpdate(gameID):
 	return updateJson
 
 
-@app.route("/hello")
-def hello():
-	return render_template('board_page.html')
-
-@app.route("/formtest", methods=('GET', 'POST'))
-def board():
-	key = random.randint(0,1000)
-
-	form = MakeMoveForm()
-	print("Here")
-	if request.method == "POST":
-		result = request.form
-		print(result)
-		print(result['row'])
-		r.incrby("score", result['row'])
-		print(r.get("score"))
-		return redirect(url_for('bio'))
-	return render_template('basic_form.html', form=form, key=key)
-
-
-@app.route("/bio")
-def bio():
-	return "Karl is a cool dude"
-
-
-@app.route("/XMLTest", methods=("GET", "POST"))
-def XMLTest():
-	if not r.exists('player1key'):
-		r.set('player1key', random.randint(0,1000))	
-	
-	print(r.get('player1key'))
-	if request.method == "POST":
-		print("In Post Request")
-		print(request.data)
-		print(type(request.data))
-		newObj = json.loads(request.data)
-		print(newObj["skey"])
-		r.incrby("score", newObj["tile"])
-		return redirect(url_for("update"))
-		
-		
-
-	
-	return render_template('HTTPTest.html', key=r.get('player1key').decode('utf-8'))
-
-
-@app.route("/XMLTest/update")
-def update():
-	state = {
-	"current":  str(r.get("score")),
-	"phase" : "Playing"
-	}
-	obj = json.dumps(state)
-	print(obj)
-	print(type(obj))
-	return obj
 
 
 if __name__ == "__main__":
